be6efe5b7b74c0caa12a0a27aeeb58c5eee475b6
leycm <leycm@proton.me>
2025-10-17 18:24:23 +0200

    Create Template commit

---
+++ tmp-api/src/main/java/de/leycm/template/TemplateApiModule.java
@@ -0,0 +1,17 @@
+/**
+ * LECP-LICENSE NOTICE
+ * <br><br>
+ * This Sourcecode is under the LECP-LICENSE. <br>
+ * License at: <a href="https://github.com/leycm/leycm/blob/main/LICENSE">GITHUB</a>
+ * <br><br>
+ * Copyright (c) LeyCM <leycm@proton.me> <br>
+ * Copyright (c) maintainers <br>
+ * Copyright (c) contributors
+ */
+package de.leycm.template;
+
+/**
+ * TemplateApiModule
+ */
+public class TemplateApiModule {
+}
---
+++ tmp-common/src/main/java/de/leycm/template/TemplateApiBootstrap.java
@@ -0,0 +1,18 @@
+/**
+ * LECP-LICENSE NOTICE
+ * <br><br>
+ * This Sourcecode is under the LECP-LICENSE. <br>
+ * License at: <a href="https://github.com/leycm/leycm/blob/main/LICENSE">GITHUB</a>
+ * <br><br>
+ * Copyright (c) LeyCM <leycm@proton.me> <br>
+ * Copyright (c) maintainers <br>
+ * Copyright (c) contributors
+ */
+package de.leycm.template;
+
+/**
+ * TemplateApiBootstrap
+ */
+public class TemplateApiBootstrap {
+
+}
